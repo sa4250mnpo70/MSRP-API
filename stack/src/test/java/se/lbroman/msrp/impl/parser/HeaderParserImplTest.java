6937f780b45011552d9a08e073f5d605c8c570db
Leonard Broman <leonard.broman@gmail.com>
2010-01-25 22:08:09 +0100

    Imported tests

--- stack/src/test/java/se/lbroman/msrp/impl/parser/HeaderParserImplTest.java
+++ stack/src/test/java/se/lbroman/msrp/impl/parser/HeaderParserImplTest.java
@@ -2,6 +2,7 @@ package se.lbroman.msrp.impl.parser;
 
 import org.junit.Test;
 
+
 import static org.junit.Assert.*;
 
 import se.lbroman.msrp.data.header.ByteRangeHeader;
@@ -10,6 +11,7 @@ import se.lbroman.msrp.data.header.MessageIDHeader;
 import se.lbroman.msrp.data.header.StatusHeader;
 import se.lbroman.msrp.impl.data.header.MsrpHeaderImpl;
 import se.lbroman.msrp.impl.data.header.RawMsrpHeader;
+import se.lbroman.msrp.impl.data.header.StatusHeaderImpl;
 import se.lbroman.msrp.impl.exception.ParseErrorException;
 
 public class HeaderParserImplTest {
@@ -48,11 +50,58 @@ public class HeaderParserImplTest {
     
     @Test
     public void parseStatusHeader() throws ParseErrorException {
-        MsrpHeaderImpl header = parser.createHeader("Status: 000 200");
+        String text = "Status: 000 200";
+        MsrpHeaderImpl header = parser.createHeader(text);
         assertTrue(header instanceof StatusHeader);
         StatusHeader h = (StatusHeader) header;
         assertEquals(0, h.getNameSpace());
         assertEquals(200, h.getCode());
+        assertEquals(text,h.encode());
+    }
+    
+    @Test
+    public void parseWithCommentSpace() throws ParseErrorException {
+        String text = "Status: 000 123 This is not so bad ";
+        MsrpHeaderImpl header = parser.createHeader(text);
+        assertTrue(header instanceof StatusHeader);
+        StatusHeader h = (StatusHeader) header;
+        assertEquals(0, h.getNameSpace());
+        assertEquals(123, h.getCode());
+        assertEquals(text,h.encode() + " ");
+    }
+
+    @Test
+    public void constructorInt() {
+        StatusHeaderImpl h = new StatusHeaderImpl();
+        h.setCode(123);
+        assertEquals(0, h.getNameSpace());
+        assertEquals(123, h.getCode());
+        assertEquals("Status: 000 123", h.encode());
+    }
+
+    @Test
+    public void constructorIntString() {
+        StatusHeaderImpl h = new StatusHeaderImpl(123, "This is not so bad");
+        assertEquals(0, h.getNameSpace());
+        assertEquals(123, h.getCode());
+        assertEquals("This is not so bad", h.getComment());
+        assertEquals("Status: 000 123 This is not so bad", h.encode());
+    }
+
+    @Test
+    public void copyConstructor() {
+        StatusHeaderImpl h = new StatusHeaderImpl(123, "This is not so bad");
+        StatusHeaderImpl h2 = new StatusHeaderImpl(h);
+        assertEquals(h, h2);
+        assertEquals(h.encode(), h2.encode());
+    }
+
+    @Test
+    public void copy() {
+        StatusHeaderImpl h = new StatusHeaderImpl(123, "This is not so bad");
+        StatusHeaderImpl h2 = h.clone();
+        assertEquals(h, h2);
+        assertEquals(h.encode(), h2.encode());
     }
     
     @Test
