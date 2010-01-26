8400dd6f2c0f885ef9b4665f5b590c09c4e63365
Leonard Broman <leonard.broman@gmail.com>
2010-01-26 19:57:28 +0100

    Added tests for to-path as well

--- stack/src/test/java/se/lbroman/msrp/impl/parser/ParsePathHeadersTest.java
+++ stack/src/test/java/se/lbroman/msrp/impl/parser/ParsePathHeadersTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+
 import se.lbroman.msrp.impl.data.header.FromPathHeaderImpl;
+import se.lbroman.msrp.impl.data.header.ToPathHeaderImpl;
 import se.lbroman.msrp.impl.exception.ParseErrorException;
 
 
@@ -43,8 +45,31 @@ public class ParsePathHeadersTest {
         String leg1 = "msrp://example.com/;tcp";
         String leg2 = "msrp://host.com/897648;tcp";
         String text = "From-Path: " + leg1 + "\r\n" + leg2;
-        FromPathHeaderImpl h = (FromPathHeaderImpl) parser.createHeader(text);
+        parser.createHeader(text);
     }
+    
+    @Test
+    public void parseOneLeg() throws ParseErrorException {
+        String leg1 = "msrp://example.com/;tcp";
+        // String leg2 = "msrp://host.com/897648;tcp";
+        String text = "To-Path: " + leg1;
+        ToPathHeaderImpl h = (ToPathHeaderImpl) parser.createHeader(text);
+        assertEquals(text, h.encode());
+        assertEquals(leg1, h.getDestination().encode());
+        assertEquals(leg1, h.getNextHop().encode());
+    }
+
+    @Test
+    public void parseTwoLegs() throws ParseErrorException {
+        String leg1 = "msrp://example.com/;tcp";
+        String leg2 = "msrp://host.com/897648;tcp";
+        String text = "To-Path: " + leg1 + " " + leg2;
+        ToPathHeaderImpl h = (ToPathHeaderImpl) parser.createHeader(text);
+        assertEquals(text, h.encode());
+        assertEquals(leg2, h.getDestination().encode());
+        assertEquals(leg1, h.getNextHop().encode());
+    }
+    
     
     
 }
